77fbe8fd96936b29273c450fac43269722a6e24b
Jonas Honecker <honeckerjonas@gmail.com>
2024-04-18 11:48:05 +0200

    Implemented test for getY and getY method.

--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -15,4 +15,16 @@ public class PlayerCharacterTest {
         int expected = 0;
         assertEquals(expected, actual);
     }
+
+    @Test
+    void getYWhenCalledWithNoArgumentsReturnIntZero(){
+        // GIVEN
+
+        // WHEN
+        int actual = PlayerCharacter.getY();
+
+        // THEN
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
