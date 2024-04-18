248d3f4e015a822657481a92b7e7f848088b4131
Jonas Honecker <honeckerjonas@gmail.com>
2024-04-18 11:36:00 +0200

    Added test

---
+++ src/test/java/PlayerCharacterTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerCharacterTest {
+
+    @Test
+    void getXWhenCalledWithNoArgumentsReturnIntZero(){
+        // GIVEN
+
+        // WHEN
+        int actual = PlayerCharacter.getX();
+
+        // THEN
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
