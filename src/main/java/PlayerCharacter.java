80f9509b039a4fb22caa99380f7307533d69c3a9
Jonas Honecker <honeckerjonas@gmail.com>
2024-04-18 12:04:42 +0200

    added new method move and respective tests.

--- src/main/java/PlayerCharacter.java
+++ src/main/java/PlayerCharacter.java
@@ -1,8 +1,17 @@
+import java.util.Objects;
+
 public class PlayerCharacter {
     public static int getX() {
         return 0;
     }
     public static int getY() {
         return 0;
+    }
+    public static int move(String keyPressed) {
+        if (Objects.equals(keyPressed, "W")) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -27,4 +27,17 @@ public class PlayerCharacterTest {
         int expected = 0;
         assertEquals(expected, actual);
     }
+
+    @Test
+    void moveWhenKeyWPressedReturnIntOne(){
+        // GIVEN
+        String keyPressed = "W";
+
+        // WHEN
+        int actual = PlayerCharacter.move(keyPressed);
+
+        // THEN
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
 }
